9c499e3b02b2f48e812074882fad5420354bb9d2
Chitolina123 <ezequielchitolina@gmail.com>
2025-07-15 15:39:26 -0300

    pasta components adicionada, junto com o footer

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Footer.css'; // Importa o CSS externo
+
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <footer className="footer">
+        <div className="footer-left">
+          <img
+            src="\" // Substitua com o caminho correto da logo
+            alt="Instituto Federal"
+            className="if-logo"
+          />
+          <img src="/IFC Con.png" alt="Instituto Federal" className="if-logo" />
+          <div className="if-text">
+            <strong>INSTITUTO FEDERAL</strong><br />
+            Catarinense<br />
+            Campus Concórdia
+          </div>
+        </div>
+
+        <div className="footer-right">
+          <div className="footer-copy">
+            © 2025 grupo 3 PI. Todos os direitos reservados.
+          </div>
+          <div className="footer-pattern">
+            oioioioioioioioioioioioioioioioioioioioio<br />
+            ioioioioioioioioioioioioioioioioioioioioi<br />
+            oioioioioioioioioioioioioioioioioioioioioi
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
