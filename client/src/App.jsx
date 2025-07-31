3afb3f992fc1c276c64c319e7a7536fa4ddfcfa4
Pedro_Augusto <pedroaugustoas2008@gmail.com>
2025-07-31 11:12:03 -0300

    Feature: Rotas adicionadas ao

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-import "./App.css";
-
+// import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/login"
+import CadPaciente from "./pages/Pacientes/CadastroPaciente/cadastropaciente"
+import Consulta from "./pages/Pacientes/Consulta/consulta"
+import Historico from "./pages/Pacientes/Historico/historico";
+import ListPacientes from "./pages/Pacientes/Lista/listapacientes";
+import CadEnfermeiro from "./pages/CadastroEnfermeiro/cadastroenfermeiro";
 
 function App() {
-
   return (
     <>
-      <h1>Enfermaria IFC</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastropaciente" element={<CadPaciente />} />
+          <Route path="/cadastroenfermeiro" element={<CadEnfermeiro />} />
+          <Route path="/consulta" element={<Consulta />} />
+          <Route path="/historico" element={<Historico />} />
+          <Route path="/listapacientes" element={<ListPacientes />} />
+        </Routes>
+      </Router>
     </>
   );
 }
