142486bb069b02a1603394fe0bbe2d9e386ec440
João Vitor Pramio <joaovitorp7777@gmail.com>
2025-08-04 16:47:46 -0300

    Add files via upload
    Foi adicionado as variáveis de botão

---
+++ client/src/components/botao/Botao.jsx
@@ -0,0 +1,9 @@
+import './Botao.css';
+
+export default function Botao({ children, variante = "claro", ...props }) {
+  return (
+    <button className={`botao ${variante}`} {...props}>
+      {children}
+    </button>
+  );
+}
