40124039a55d97f5fe820557f3effed08efe6da7
João Vitor Pramio <joaovitorp7777@gmail.com>
2025-08-12 15:06:10 -0300

    criação da pagina de login, falta componente footer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './pages/CadastroEnfermeiro/cadastroenfermeiro.jsx'
+import App from './pages/Login/login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
