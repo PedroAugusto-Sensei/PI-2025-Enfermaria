1f20bacdda216851df38bb26d0054204b71776bd
João Vitor Pramio <joaovitorp7777@gmail.com>
2025-08-12 15:19:54 -0300

    pagina de login arrumada

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './pages/Pacientes/Lista/listapacientes'
+import App from './pages/Login/login.jsx'
 
 
 createRoot(document.getElementById('root')).render(
