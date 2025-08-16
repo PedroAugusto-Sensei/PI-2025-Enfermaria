998d953f81bad58e8650bfcdada917f4503d15bf
João Vitor Pramio <joaovitorp7777@gmail.com>
2025-08-16 19:57:37 -0300

    correção do component search bar e botão

--- client/src/components/barrapesquisa/barrapesquisa.jsx
+++ client/src/components/barrapesquisa/barrapesquisa.jsx
@@ -28,7 +28,7 @@ export default function SearchBar({ placeholder = "Pesquisar", buttonLabel = "Bu
       <button className="sb-button" type="submit" aria-label={buttonLabel}>
         <span className="sb-button-text">{buttonLabel}</span>
 
-        {/* ícone SVG (sem dependências externas) */}
+        {/* ícone SVG */}
         <svg className="sb-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
           <circle cx="11" cy="11" r="6" stroke="currentColor" strokeWidth="2" fill="none" />
           <path d="M20 20 L16.65 16.65" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './pages/Login/login.jsx'
-import Teste from './pages/Pacientes/Historico/historico.jsx'
+import Teste from './pages/Pacientes/Lista/listapacientes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
