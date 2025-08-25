aaa9d04a3e4c90a4b7a9e52ab1d52e9f2b689cde
PedroAugusto-Sensei <pedroaugustoas2008@gmail.com>
2025-08-25 19:40:16 -0300

    Feature: Botão no Header para voltar as páginas.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import Botao from "../botao/Botao.jsx";
 
 import "./Header.css";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleVoltar = () => {
+    if (location.pathname === "/listapacientes") {
+      // Já está na página inicial, não faz nada
+      return;
+    } else {
+      navigate(-1); // Volta para a página anterior
+    }
+  };
+
   return (
     <header className="header">
       <h1>Enfermaria IFC</h1>
+        <Botao 
+          onClick={handleVoltar}
+          variante="claro"
+        >
+          ← Voltar
+        </Botao>
     </header>
   );
 };
